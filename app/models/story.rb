868bfc4b614f5fc31c32308369b5ab07724ecc08
Jim Tsai <jim.h.tsai@gmail.com>
2014-12-01 11:31:10 -0800

    list is ensured first before order

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,19 +1,21 @@
 class Story < ActiveRecord::Base
 	belongs_to :project
-	after_initialize :ensure_order, :ensure_list
+	after_initialize :ensure_list, :ensure_order
 
 	private
 	def ensure_order
-		sql = <<-sql
+		if self.order == nil
+			sql = <<-sql
 			SELECT
 				MAX(stories.order) AS last_order
 			FROM
 				stories
 			WHERE
 				stories.list = '#{self.list}'
-		sql
-		record = ActiveRecord::Base.connection.execute(sql)
-		self.order ||= record[0]['last_order'].to_i + 1
+			sql
+			record = ActiveRecord::Base.connection.execute(sql)
+			self.order = record[0]['last_order'].to_i + 1
+		end
 	end
 
 	def ensure_list
