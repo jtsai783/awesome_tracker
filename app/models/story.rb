05342c8299d3839f3b0b6a8bf9089e6c8179337c
Jim Tsai <jim.h.tsai@gmail.com>
2014-12-01 11:24:28 -0800

    ensure order and list on database story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,9 +1,22 @@
 class Story < ActiveRecord::Base
 	belongs_to :project
-	after_initialize :ensure_order
+	after_initialize :ensure_order, :ensure_list
 
 	private
 	def ensure_order
-		self.order ||= Story.maximum('order') + 1
+		sql = <<-sql
+			SELECT
+				MAX(stories.order) AS last_order
+			FROM
+				stories
+			WHERE
+				stories.list = '#{self.list}'
+		sql
+		record = ActiveRecord::Base.connection.execute(sql)
+		self.order ||= record[0]['last_order'].to_i + 1
+	end
+
+	def ensure_list
+		self.list ||= 'icebox'
 	end
 end
